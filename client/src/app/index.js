1c8ca499f54d21fca5b4f1783e025c77c6d4493a
katrin-ibrahim <katrinemadfathy@gmail.com>
2021-11-05 17:47:43 +0200

    added flight card component

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,10 +1,12 @@
 
 import '../style/App.css';
+import Flight from '../components/Flight';
 
 function App() {
   return (
     <div className="App">
       <h1>helloo</h1>
+      <Flight/>
     </div>
   );
 }
---
+++ client/src/components/Flight.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Flight extends React.Component{
+    render(){
+        return(
+            <div className = "">
+                {/* flight number, departure and arrival times, dates, number of Economy seats, number of Business class seats, and airport */}
+                <h2> Flight Number : </h2>
+                <p> Departure time :</p>
+                <p> Arrival time :</p>
+                <p> Flight Date :</p>
+                <p> Economy Class Seats : </p>
+                <p> Business Class Seats : </p>
+                <p> Airport : </p>
+                
+            </div>
+        )
+    }
+  }
+  export default Flight;
