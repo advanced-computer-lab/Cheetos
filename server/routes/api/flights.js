d0de7486b792c63c34f242db42ed42156606b780
MaryseFahim <marizefahim@gmail.com>
2021-11-27 17:08:23 +0200

    Sprint1updated

--- server/routes/api/flights.js
+++ server/routes/api/flights.js
@@ -2,16 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const flightController = require('../../controllers/flightController')
+const flightController = require("../../controllers/flightController");
 
 const Flight = require("../../models/Flight");
 
-router.get("/", flightController.getFlights)
+router.get("/", flightController.getFlights);
 
-router.post("/create",flightController.createFlight )
+router.post("/create", flightController.createFlight);
 
-router.delete("/delete",flightController.deleteFlight )
+router.delete("/delete/:id", flightController.deleteFlight);
 
-router.put('/update',flightController.updateFlight)
+router.put("/update/:id", flightController.updateFlight);
 
 module.exports = router;
