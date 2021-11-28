341ae84d3cb0b61c718d33739d48b1b97955497a
Mark-Malak <markmalak71@gmail.com>
2021-11-28 21:16:18 +0200

    Created Trip component UI

--- client/src/components/MyHeader.js
+++ client/src/components/MyHeader.js
@@ -7,118 +7,126 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 
 export default class MyHeader extends Component {
-    handleSearch(){
+  state = {
+    myValue: 333
+  }
+  handleSearch() {
 
-    }
-    handleProfileClick(){
+  }
+  handleProfileClick() {
 
-    }
-    render() {
-        return (
-            <div className="admin-header logo-buttons-search">
-                
-                    <div className="logo-buttons"> <Button
-            onClick={this.handleProfileClick}
-            style={{
-             
-              backgroundColor: "#37A1E2",
-              width: "12%",
-              height: "7vh",
-              fontSize: "medium",
+  }
+  handleHeho(e) {
+    this.setState({
+      myValue: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="admin-header logo-buttons-search">
 
-            }}
-            variant="contained"
-          >
-            <PersonIcon style={{marginRight:"5px" , fontSize:"xx-large"}}/>Profile{" "}
-          </Button>
-          </div>
-                    <div className="search-bar">
-                        {/* n of passengers , dep airport , arr air , dep date ,arr date ,cabin class  */}
-                <Form.Group style={{ flexGrow: 1 ,width:"12%" }} className="mb-3">
-                    <Form.Label>Adult passengers: </Form.Label>
-                    <Form.Control
+        <div className="logo-buttons"> <Button
+          onClick={this.handleProfileClick}
+          style={{
 
-                    type="number"
-                         placeholder="ex: 1"
-                    // value={}
-                    // name=""
-                    onChange={this.handleSearch.bind(this)}
-                    />
-                </Form.Group>
+            backgroundColor: "#37A1E2",
+            width: "12%",
+            height: "7vh",
+            fontSize: "medium",
 
-                <Form.Group style={{ flexGrow: 1 ,width:"12%"}} className="mb-3">
-                    <Form.Label>Child passengers: </Form.Label>
-                    <Form.Control
-                    type="number"
-                        placeholder="ex: 2"
-                    // value={}
-                    // name=""
-                    onChange={this.handleSearch.bind(this)}
-                    />
-                </Form.Group>
+          }}
+          variant="contained"
+        >
+          <PersonIcon style={{ marginRight: "5px", fontSize: "xx-large" }} />Profile{" "}
+        </Button>
+        </div>
+        <div className="search-bar">
+          {/* n of passengers , dep airport , arr air , dep date ,arr date ,cabin class  */}
+          <Form.Group style={{ flexGrow: 1, width: "12%" }} className="mb-3">
+            <Form.Label>Adult passengers: </Form.Label>
+            <Form.Control
 
-                <Form.Group style={{ flexGrow: 1 ,width:"12%"}} className="mb-3">
-                 <Form.Label>Departure Airport: </Form.Label>
-                 <Form.Control
-                    type="text"
-                    placeholder="ex: LAX"
-                    // value={}
-                    // name=""
-                    onChange={this.handleSearch.bind(this)}
-                />
-                </Form.Group>
+              type="number"
+              placeholder="ex: 1"
+              value={this.state.myValue}
+              // name=""
+              onChange={this.handleHeho.bind(this)}
+            />
+          </Form.Group>
 
-              <Form.Group style={{ flexGrow: 1 ,width:"12%"}} className="mb-3">
-                <Form.Label>Arrival airport: </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="ex: CAI"
-                //   value={}
-                //   name=""
-                  onChange={this.handleSearch.bind(this)}
-                />
-              </Form.Group>
+          <Form.Group style={{ flexGrow: 1, width: "12%" }} className="mb-3">
+            <Form.Label>Child passengers: </Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="ex: 2"
+              // value={}
+              // name=""
+              onChange={this.handleSearch.bind(this)}
+            />
+          </Form.Group>
 
-              <Form.Group style={{ flexGrow: 1 ,width:"12%"}} className="mb-3">
-                <Form.Label>Departure date: </Form.Label>
-                <Form.Control
-                  type="date"
-                  placeholder="Date (dd/MM/YY). . ."
-                //   value={}
-                //   name=""
-                  onChange={this.handleSearch.bind(this)}
-                />
-              </Form.Group>
+          <Form.Group style={{ flexGrow: 1, width: "12%" }} className="mb-3">
+            <Form.Label>Departure Airport: </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="ex: LAX"
+              // value={}
+              // name=""
+              onChange={this.handleSearch.bind(this)}
+            />
+          </Form.Group>
 
+          <Form.Group style={{ flexGrow: 1, width: "12%" }} className="mb-3">
+            <Form.Label>Arrival airport: </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="ex: CAI"
+              //   value={}
+              //   name=""
+              onChange={this.handleSearch.bind(this)}
+            />
+          </Form.Group>
 
-              <Form.Group style={{ flexGrow: 1 ,width:"12%"}} className="mb-3">
-                <Form.Label>Arrival date: </Form.Label>
-                <Form.Control
-                  style={{ width: "" }}
-                  type="date"
-                  placeholder="Date (dd/MM/YY). . ."
-                //   value={}
-                //   name=""
-                  onChange={this.handleSearch.bind(this)}
-                />
-              </Form.Group>
+          <Form.Group style={{ flexGrow: 1, width: "12%" }} className="mb-3">
+            <Form.Label>Departure date: </Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Date (dd/MM/YY). . ."
+              //   value={}
+              //   name=""
+              onChange={this.handleSearch.bind(this)}
+            />
+          </Form.Group>
 
 
-              <Form.Group style={{ flexGrow: 1 }} className="mb-3">
-                <Form.Label>Cabin class: </Form.Label>
-                <Form.Select aria-label="Default select example">
-                <option>click to select</option>
-                <option value="1">Economy</option>
-                <option value="2">Business class</option>
-                <option value="3">First Class</option>
-              </Form.Select>
-              </Form.Group>
+          <Form.Group style={{ flexGrow: 1, width: "12%" }} className="mb-3">
+            <Form.Label>Arrival date: </Form.Label>
+            <Form.Control
+              style={{ width: "" }}
+              type="date"
+              placeholder="Date (dd/MM/YY). . ."
+              //   value={}
+              //   name=""
+              onChange={this.handleSearch.bind(this)}
+            />
+          </Form.Group>
 
 
-                    </div>
+          <Form.Group style={{ flexGrow: 1 }} className="mb-3">
+            <Form.Label>Cabin class: </Form.Label>
+            <Form.Select aria-label="Default select example">
+              <option>click to select</option>
+              <option value="1">Economy</option>
+              <option value="2">Business class</option>
+              <option value="3">First Class</option>
+            </Form.Select>
+          </Form.Group>
 
-                </div>
-            
-        )
-    }
+
+        </div>
+
+      </div>
+
+    )
+  }
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,114 +6,121 @@ import EditIcon from '@mui/icons-material/Edit';
 import img from '../images/background.jpg';
 import { fontSize } from '@mui/system';
 import MyHeader from '../components/MyHeader';
+import Trip from '../components/Trip';
 
 export default class Profile extends Component {
-  
-    state ={
-        editName : false,
-        editPassport : false,
-        editEmail : false,
-     }
-    handleEditName() {
-     
-        console.log(this.state.id)
-        this.setState(
-          {
-            editName: !this.state.editName
-          }
-        )
+
+  state = {
+    editName: false,
+    editPassport: false,
+    editEmail: false,
+  }
+
+  handleEditName() {
+
+    console.log(this.state.id)
+    this.setState(
+      {
+        editName: !this.state.editName
       }
+    )
+  }
 
-      handleEditPassport() {
-     
-        console.log(this.state.id)
-        this.setState(
-          {
-            editPassport: !this.state.editPassport
-          }
-        )
+  handleEditPassport() {
+
+    console.log(this.state.id)
+    this.setState(
+      {
+        editPassport: !this.state.editPassport
       }
-      handleEditEmail(){
-        console.log(this.state.id)
-        this.setState(
-          {
-            editEmail: !this.state.editEmail
-          }
-        )
+    )
+  }
+  handleEditEmail() {
+    console.log(this.state.id)
+    this.setState(
+      {
+        editEmail: !this.state.editEmail
       }
+    )
+  }
 
-       handleSave() {
-        
-    
-          window.location.reload();
-        
-    
-      }
+  handleSave() {
 
-    render() {
-       
-        const { fname,lname,passport,email} = this.props
-        const{editName,editPassport,editEmail} = this.state;
-        return (
-            <div className = "flex-col-profile" >
-                <div className="header"> <MyHeader /></div>
-                <div className="flex-row-profile">
-                    
-                    <div className = "profile">
-                        <table style={{width:"100%"}}>
-                            <tr className="user-name" >
-                                {!editName?
-                                <>
-                                <th><AccountCircleIcon style ={{ color: "#12228F", fontSize:"10rem" }} /></th>
-                                <td>{fname} <br/>{lname}</td>
-                                <td><EditIcon className="icon" onClick={this.handleEditName.bind(this)} /></td>
-                                </>:
-                                <> 
-                                <th><AccountCircleIcon style ={{ color: "#12228F", fontSize:"13rem" }} /></th>
-                                <td><Form.Control style={{ width: '60%'  }} size="sm" name="firstName" type="text" placeholder="first name" value={fname}/>
-                                <Form.Control style={{ width: '60%'  }} size="sm" name="lastName" type="text" placeholder="last name" value={lname}/></td>
-                                <td> <p onClick={this.handleSave.bind(this)} style = {{margin : '0' , fontWeight : 'bold', fontSize:'medium'}}className="icon"> save </p></td>
-                                </>}
-                            </tr>
-                        
-                            <tr>
-                                {!editPassport?
-                                 <>
-                                <th>Passport Number:</th>
-                                <td>{passport}</td>
-                                <td><EditIcon className="icon" onClick={this.handleEditPassport.bind(this)} /></td>
-                                </>:
-                                
-                                <>
-                                 <th>Passport Number:</th>
-                                <td><Form.Control style={{ width: '60%'  }} size="sm" name="passport" type="text" placeholder="Passport Number" value={passport}/></td>
-                                <td> <p onClick={this.handleSave.bind(this)} style = {{margin : '0' , fontWeight : 'bold', fontSize:'medium'}}className="icon"> save </p></td>
 
-                                </>}
-                            </tr>
+    window.location.reload();
 
-                            <tr>
-                                {!editEmail?
-                                <>
-                                <th> Email:</th>
-                                <td>{email}</td>
-                                <td><EditIcon className="icon" onClick={this.handleEditEmail.bind(this)} /></td>
-                                </>:
-                                 <>
-                                 <th> Email:</th>
-                                 <td><Form.Control style={{ width: '60%'  }} size="sm" name="email" type="text" placeholder="Email" value={email}/></td>
-                                 <td><p onClick={this.handleSave.bind(this)} style = {{margin : '0' , fontWeight : 'bold', fontSize:'medium'}}className="icon"> save </p></td>
-                                 </>
-                                }
-                            </tr>
-                        </table>
-                    </div>
-                    
-                   
-                </div>
-              
-            </div>
-        )
-    }
+
+  }
+
+  render() {
+
+    const { fname, lname, passport, email } = this.props
+    const { editName, editPassport, editEmail } = this.state;
+    return (
+      <div className="flex-col-profile" >
+        <div className="header"> <MyHeader /></div>
+        <div className="flex-row-profile">
+
+          {/* <div className="profile">
+            <table style={{ width: "100%" }}>
+              <tr className="user-name" >
+                {!editName ?
+                  <>
+                    <th><AccountCircleIcon style={{ color: "#12228F", fontSize: "10rem" }} /></th>
+                    <td>{fname} <br />{lname}</td>
+                    <td><EditIcon className="icon" onClick={this.handleEditName.bind(this)} /></td>
+                  </> :
+                  <>
+                    <th><AccountCircleIcon style={{ color: "#12228F", fontSize: "13rem" }} /></th>
+                    <td><Form.Control style={{ width: '60%' }} size="sm" name="firstName" type="text" placeholder="first name" value={fname} />
+                      <Form.Control style={{ width: '60%' }} size="sm" name="lastName" type="text" placeholder="last name" value={lname} /></td>
+                    <td> <p onClick={this.handleSave.bind(this)} style={{ margin: '0', fontWeight: 'bold', fontSize: 'medium' }} className="icon"> save </p></td>
+                  </>}
+              </tr>
+
+              <tr>
+                {!editPassport ?
+                  <>
+                    <th>Passport Number:</th>
+                    <td>{passport}</td>
+                    <td><EditIcon className="icon" onClick={this.handleEditPassport.bind(this)} /></td>
+                  </> :
+
+                  <>
+                    <th>Passport Number:</th>
+                    <td><Form.Control style={{ width: '60%' }} size="sm" name="passport" type="text" placeholder="Passport Number" value={passport} /></td>
+                    <td> <p onClick={this.handleSave.bind(this)} style={{ margin: '0', fontWeight: 'bold', fontSize: 'medium' }} className="icon"> save </p></td>
+
+                  </>}
+              </tr>
+
+              <tr>
+                {!editEmail ?
+                  <>
+                    <th> Email:</th>
+                    <td>{email}</td>
+                    <td><EditIcon className="icon" onClick={this.handleEditEmail.bind(this)} /></td>
+                  </> :
+                  <>
+                    <th> Email:</th>
+                    <td><Form.Control style={{ width: '60%' }} size="sm" name="email" type="text" placeholder="Email" value={email} /></td>
+                    <td><p onClick={this.handleSave.bind(this)} style={{ margin: '0', fontWeight: 'bold', fontSize: 'medium' }} className="icon"> save </p></td>
+                  </>
+                }
+              </tr>
+            </table>
+          </div> */}
+           
+
+        </div>
+        <Trip/>
+            <Trip/>
+            <Trip/>
+            <Trip/>
+            <Trip/>
+            <Trip/>
+      </div>
+    )
+  }
 }
 //export default Profile;
