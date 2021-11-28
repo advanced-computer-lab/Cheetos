b7354b25413185b5e92c7b704fb821b9f3860823
MaryseFahim <marizefahim@gmail.com>
2021-11-28 21:10:03 +0200

    updated routes

--- server/app.js
+++ server/app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
-const connectDB = require('./db.js')
-
+const connectDB = require("./db.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
+const flightRoutes = require("./routes/api/flights");
+const userRoutes = require("./routes/api/users");
+const reservationRoutes = require("./routes/api/reservations");
 
-const routes = require("./routes/api/flights");
-
-connectDB()
-app.use("/api", routes);
+connectDB();
+app.use("/api/flight", flightRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/reservation", reservationRoutes);
 
 const port = process.env.PORT;
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,101 +1,87 @@
-const User = require("../models/User")
-const Flight = require("../models/User")
+const User = require("../models/User");
+const Flight = require("../models/User");
 
 updateUser = async (req, res) => {
-    const body = req.body
+  const body = req.body;
 
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            error: 'You must provide a body to update',
-        })
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  User.findOne({ _id: req.params.id }, (err, user) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "User not found!",
+      });
     }
+    console.log(user);
 
-    User.findOne({ _id: req.params.id }, (err, flight) => {
-        if (err) {
-            return res.status(404).json({
-                err,
-                message: 'User not found!',
-            })
-        }
-        console.log(user)
+    user.FirstName = body.FirstName;
 
-        user.FirstName = body.FirstName
+    user.LastName = body.LastName;
 
+    user.HomeAddress = body.HomeAddress;
 
-        user.LastName = body.LastName
+    user.CountryCode = body.CountryCode;
 
+    user.Email = body.Email;
 
-        user.HomeAddress = body.HomeAddress
+    user.TelephoneNumber = body.TelephoneNumber;
 
+    user.PassportNumber = body.PassportNumber;
 
-        user.CountryCode = body.CountryCode
-
-
-        user.Email = body.Email
-
-        user.TelephoneNumber = body.TelephoneNumber
-
-
-        user.PassportNumber = body.PassportNumber
-
-
-        user
-            .save()
-            .then(() => {
-                return res.status(200).json({
-                    success: true,
-                    id: flight.id,
-                    message: 'User updated!',
-                })
-            }
-            )
-            .catch(error => {
-                return res.status(404).json({
-                    error,
-                    message: 'User not updated!',
-                })
-            })
-
-    })
-
-}
+    user
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: user.id,
+          message: "User updated!",
+        });
+      })
+      .catch((error) => {
+        return res.status(404).json({
+          error,
+          message: "User not updated!",
+        });
+      });
+  });
+};
 
 deleteUser = async (req, res) => {
-    await User.findByIdAndDelete({ _id: req.params.id }, (err, flight) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+  await User.findByIdAndDelete({ _id: req.params.id }, (err, user) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
 
-        if (!user) {
-            return res
-                .status(404)
-                .json({ success: false, error: `User not found` })
-        }
+    if (!user) {
+      return res.status(404).json({ success: false, error: `User not found` });
+    }
 
-        return res.status(200).json({ success: true, data: flight })
-    })
-        .catch(err => console.log(err))
-}
+    return res.status(200).json({ success: true, data: user });
+  }).catch((err) => console.log(err));
+};
 
 createUser = (req, res) => {
-    User.create(req.body)
-        .then((user) => res.json({ msg: "User added successfully", data: req.body }))
-        .catch((err) =>
-            res.status(400).json({ error: "Unable to add this user" })
-        )
-}
+  User.create(req.body)
+    .then((user) =>
+      res.json({ msg: "User added successfully", data: req.body })
+    )
+    .catch((err) => res.status(400).json({ error: "Unable to add this user" }));
+};
 getUser = (req, res) => {
-    User.find()
-        .then((users) => res.json(users))
-        .catch((err) =>
-            res.status(404).json({ nousersfound: "No User found" })
-        );
-}
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(404).json({ nousersfound: "No User found" }));
+};
 
 module.exports = {
-    updateUser,
-    deleteUser,
-    createUser,
-    getUser
-}
+  updateUser,
+  deleteUser,
+  createUser,
+  getUser,
+};
