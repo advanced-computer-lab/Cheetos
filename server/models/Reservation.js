0e774a292d49346979aad1b52a76f48b2eb39974
MaryseFahim <marizefahim@gmail.com>
2021-12-02 00:52:05 +0200

    Added email

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -1,42 +1,90 @@
 const mongoose = require("mongoose");
- const ReservationSchema = new mongoose.Schema({
-     UserId : {
-         type: mongoose.Schema.Types.ObjectId, 
-         ref: 'User',
-         required: true,
-     },
+const nodemailer = require("nodemailer");
 
-     Reservation: [{
-        FlightId: {
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'Flight',
-            required: true
-        },
-        
-        CabinClass: {
-            type: String,
-            required: true
-        },
+const ReservationSchema = new mongoose.Schema({
+  UserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 
-        ChosenSeat: {
-            type: String,
-            required: true
-        },
+  Reservation: [
+    {
+      FlightId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Flight",
+        required: true,
+      },
 
-        Passenger: {
-            type: String,
-            required: true
-        },
+      CabinClass: {
+        type: String,
+        required: true,
+      },
 
-        Price: {
-            type: Number,
-            required: true
-        },
+      ChosenSeat: {
+        type: String,
+        required: true,
+      },
 
-        BaggageAllowance: {
-            type: Number,
-            required: true
+      Passenger: {
+        type: String,
+        required: true,
+      },
+
+      Price: {
+        type: Number,
+        required: true,
+      },
+
+      BaggageAllowance: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+ReservationSchema.post("save", (doc, next) => {
+  const filter = { _id: doc.Reservation[0].FlightId };
+  const update = { $inc: { "EconomySeats.AvailableSeats": -1 } };
+  console.log(doc.Reservation[0].FlightId);
+  //const update = { FlightNumber: 5 };
+  Flight.findOneAndUpdate(filter, update, { new: true }, (err, newdoc) => {
+    if (err) {
+      console.log("error");
+    }
+  });
+  next();
+});
+
+ReservationSchema.post("findOneAndDelete", (reservation, next) => {
+  User.findById(reservation.UserId, "Email", (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(user.Email);
+      let mailTransporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+          user: "cheetosmym1@outlook.com",
+          pass: "Acltestingmail.1",
+        },
+      });
+      let mailDetails = {
+        from: "cheetosmym1@outlook.com",
+        to: user.Email,
+        subject: "cancellation confirmation",
+        text: "Dear Customer  your reservation was cancelled dollar was refunded ",
+      };
+      mailTransporter.sendMail(mailDetails, function (err, data) {
+        if (err) {
+          console.log("Error Occured", err);
+        } else {
+          console.log("Email sent successfully");
         }
-     }]
+      });
+    }
+  });
+  next();
+});
 
- })
+module.exports = Reservation = mongoose.model("Reservation", ReservationSchema);
