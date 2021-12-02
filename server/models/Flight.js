6f2d35c385252402919ba9ba9b75f5215d1ab3dc
MaryseFahim <marizefahim@gmail.com>
2021-12-02 19:46:48 +0200

    seatsArray &PricChild

--- server/models/Flight.js
+++ server/models/Flight.js
@@ -28,9 +28,12 @@ const FlightSchema = new mongoose.Schema({
       type: Number,
       required: true,
     },
-    Price: {
+    PriceAdult: {
       type: Number,
       required: true,
+    },
+    PriceChild: {
+      type: Number,
     },
     Seats: [
       {
@@ -50,9 +53,12 @@ const FlightSchema = new mongoose.Schema({
       type: Number,
       required: true,
     },
-    Price: {
+    PriceAdult: {
       type: Number,
       required: true,
+    },
+    PriceChild: {
+      type: Number,
     },
     Seats: [
       {
@@ -71,9 +77,12 @@ const FlightSchema = new mongoose.Schema({
       type: Number,
       required: true,
     },
-    Price: {
+    PriceAdult: {
       type: Number,
       required: true,
+    },
+    PriceChild: {
+      type: Number,
     },
     Seats: [
       {
@@ -111,6 +120,110 @@ const FlightSchema = new mongoose.Schema({
   TripDuration: {
     type: Number,
   },
+});
+
+FlightSchema.post("save", (flight, next) => {
+  let PriceAFC = flight.FirstClassSeats.PriceAdult;
+  const update = {
+    $set: { "FirstClassSeats.PriceChild": PriceAFC * (8 / 10) },
+  };
+  //FirstClass
+  Flight.findByIdAndUpdate({ _id: flight._id }, update, { new: true })
+    .then((newflight) => {})
+    .catch((err) => res.status(400).json({ error: err }));
+
+  //BusinessClass
+  let PriceABC = flight.BusinessSeats.PriceAdult;
+  const updateB = {
+    $set: { "BusinessSeats.PriceChild": PriceABC * (8 / 10) },
+  };
+  Flight.findByIdAndUpdate({ _id: flight._id }, updateB, { new: true })
+    .then((newflight) => {})
+    .catch((err) => res.status(400).json({ error: err }));
+
+  //EconomyClass
+  let PriceAEC = flight.EconomySeats.PriceAdult;
+  const updateE = {
+    $set: { "EconomySeats.PriceChild": PriceAEC * (8 / 10) },
+  };
+  Flight.findByIdAndUpdate({ _id: flight._id }, updateE, { new: true })
+    .then((newflight) => {})
+    .catch((err) => res.status(400).json({ error: err }));
+  next();
+});
+
+FlightSchema.post("save", (flight, next) => {
+  //FirstClass
+  //get length firstclass
+  let seats = flight.FirstClassSeats.AvailableSeats;
+  let condition = seats / 4 + 1;
+  let array = [];
+  let rows = 1;
+  for (i = 1; i < condition; i++) {
+    for (let j = 0; j < 4; j++) {
+      var letter = String.fromCharCode(65 + j);
+      array.push({ Seat: letter + i, Reserved: false });
+    }
+    rows = i + 1;
+  }
+  Flight.findByIdAndUpdate(
+    { _id: flight._id },
+    {
+      $set: {
+        "FirstClassSeats.Seats": array,
+      },
+    },
+    { new: true }
+  )
+    .then((newflight) => {})
+    .catch((err) => res.status(400).json({ error: err }));
+  //BusinessClass
+  //get length Businessclass
+  let Bseats = flight.BusinessSeats.AvailableSeats;
+  condition = Bseats / 4 + rows;
+  array = [];
+  for (let i = rows; i < condition; i++) {
+    for (let j = 0; j < 4; j++) {
+      var letter = String.fromCharCode(65 + j);
+      array.push({ Seat: letter + i, Reserved: false });
+    }
+    rows = rows + i;
+  }
+  Flight.findByIdAndUpdate(
+    { _id: flight._id },
+    {
+      $set: {
+        "BusinessSeats.Seats": array,
+      },
+    },
+    { new: true }
+  )
+    .then((newflight) => {})
+    .catch((err) => res.status(400).json({ error: err }));
+
+  //Economy
+  //get length Economyclass
+  let Eseats = flight.EconomySeats.AvailableSeats;
+  condition = rows + Eseats / 4;
+  array = [];
+  for (i = rows; i < condition; i++) {
+    for (let j = 0; j < 4; j++) {
+      var letter = String.fromCharCode(65 + j);
+      array.push({ Seat: letter + i, Reserved: false });
+    }
+  }
+  Flight.findByIdAndUpdate(
+    { _id: flight._id },
+    {
+      $set: {
+        "EconomySeats.Seats": array,
+      },
+    },
+    { new: true }
+  )
+    .then((newflight) => {})
+    .catch((err) => res.status(400).json({ error: err }));
+  next();
 });
 
 module.exports = Flight = mongoose.model("Flight", FlightSchema);
