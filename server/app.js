7a4bdff9ad32cf3cc18705036c115b1450a36940
MaryseFahim <marizefahim@gmail.com>
2021-12-23 22:29:38 +0200

    payment

--- server/app.js
+++ server/app.js
@@ -13,11 +13,13 @@ app.use(bodyParser.json());
 const flightRoutes = require("./routes/api/flights");
 const userRoutes = require("./routes/api/users");
 const reservationRoutes = require("./routes/api/reservations");
+const paymentRoutes = require("./routes/api/payment");
 
 connectDB();
 app.use("/api/flight", flightRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/reservation", reservationRoutes);
+app.use("/api/payment", paymentRoutes);
 
 const port = process.env.PORT;
 
