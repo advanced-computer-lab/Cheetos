b989cb8cb809fd31d8501871c56ef817bef52fd7
MaryseFahim <marizefahim@gmail.com>
2021-12-25 22:27:45 +0200

    AdminAuth

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,11 @@ const api = axios.create({
 });
 //admin
 export const insertFlight = (payload) =>
-  api.post(`/flight/createflight`, payload);
+  api.post(`/flight/createflight`, payload, {
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
 export const getAllFlights = () => api.get(`/flight/getflights`);
 export const getFlightById = (id) => api.get(`/flight/getflight/${id}`);
 export const updateFlightbyId = (id, payload) =>
@@ -23,24 +27,18 @@ export const deleteFlightById = (id) =>
 
 //sprint 2
 export const confirmFlight = (payload) =>
-  api.post(`/reservation/createreservation`, payload,
-    {
-      headers: {
-        "x-access-token": localStorage.getItem("token"),
-      },
-    }
-  );
-
+  api.post(`/reservation/createreservation`, payload, {
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
 
 export const editReservation = (id, payload) =>
-  api.post(`/reservation/updatereservationflight/${id}`, payload,
-    {
-      headers: {
-        "x-access-token": localStorage.getItem("token"),
-      },
-    }
-  );
-
+  api.post(`/reservation/updatereservationflight/${id}`, payload, {
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
 
 //user profile
 export const getUserInfo = (id) => api.get(`/user/u/${id}`);
@@ -51,22 +49,24 @@ export const updateUserInfo = (id, payload) =>
     },
   });
 //
-export const getReservationsById = id => api.get(`/reservation/getreservation/${id}`, {
-  headers: {
-    "x-access-token": localStorage.getItem("token"),
-  },
-}
-)
-export const deleteReservationById = id => api.delete(`/reservation/deletereservation/${id}`, {
-  headers: {
-    "x-access-token": localStorage.getItem("token"),
-  },
-})
-export const payReservation = (id, payload) => api.post(`/reservation/sendmailpay/${id}`, payload, {
-  headers: {
-    "x-access-token": localStorage.getItem("token"),
-  },
-})
+export const getReservationsById = (id) =>
+  api.get(`/reservation/getreservation/${id}`, {
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
+export const deleteReservationById = (id) =>
+  api.delete(`/reservation/deletereservation/${id}`, {
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
+export const payReservation = (id, payload) =>
+  api.post(`/reservation/sendmailpay/${id}`, payload, {
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
 
 //authentication
 export const loginUser = (payload) => api.post(`/user/ulogin`, payload);
@@ -101,7 +101,7 @@ const apis = {
   validateEmail,
   validateUsername,
   updatePassword,
-  editReservation
+  editReservation,
 };
 
 export default apis;
